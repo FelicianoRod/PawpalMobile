e689356ef3460565117515bea010cfa6a7eeaae8
Feliciano <felicianorodriguez55@gmail.com>
2024-12-04 17:22:27 -0600

    Integración de una notificación al agregar una nueva mascota

--- app/src/main/java/com/example/pawpal/PawpalMovile.kt
+++ app/src/main/java/com/example/pawpal/PawpalMovile.kt
@@ -1,7 +1,33 @@
 package com.example.pawpal
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PawpalMovile: Application()
+class PawpalMovile: Application() {
+    companion object {
+        const val CHANNEL_ID = "my_channel"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Actualizaciones",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "Este canal se usa para notificar"
+            val notificationManager = this.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
